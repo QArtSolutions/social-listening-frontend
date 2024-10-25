d14383bd4b6d3b7a740c885a000311db0a06b819
Dell <Dell@SANSKAR>
2024-10-25 10:15:23 +0530

    [NEW] made signup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AppRouter from './pages/AppRouter';
 
 function App() {
+  console.log("app is rendering");
   return (
     <AppRouter />
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
-
-// import './index.css'; // Import global CSS (if any)
 import App from './App';
 
 ReactDOM.render(

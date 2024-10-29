f6faad057c56e7439556ccb37f0e738297c7a754
Dell <Dell@SANSKAR>
2024-10-29 17:23:52 +0530

    [UPDATED] mentions page by adding some features

--- src/pages/AppRouter.js
+++ src/pages/AppRouter.js
@@ -1,6 +1,7 @@
 import React, {lazy} from 'react';
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
 import Entry from '../component/entry';
+import { BrandProvider } from '../contexts/BrandContext';
 // import SignUp from '../features/auth/SignUp';
 // import BrandPage from '../pages/BrandPage';
 
@@ -17,7 +18,7 @@ const AppRouter = ({ isAuthenticated, setIsAuthenticated }) => {
       <Route exact path="/home" element={isAuthenticated ? <BrandPage setIsAuthenticated={setIsAuthenticated} /> : <Entry setIsAuthenticated={setIsAuthenticated} />} />
       <Route exact path="/signup" element={<SignUp />} />
       <Route exact path="/entry" element={<Entry setIsAuthenticated={setIsAuthenticated} />} />
-      <Route exact path="/mentions" element={<MentionsPage />} />
+      <Route exact path="/mentions" element={<MentionsPage />} /> 
       {/* Add more routes here */}
     </Routes>
   );

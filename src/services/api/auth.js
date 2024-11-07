c5dedef5f9e316d883d8901487035d143787435a
Dell <Dell@SANSKAR>
2024-11-07 20:19:18 +0530

    [UPDATED] tried resolving backend error using dev api

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -1,9 +1,10 @@
- import API_BASE_URL from '../../config.js';
+const API_URL = 'https://dev-backend.socialhear.com/'; 
+// import API_BASE_URL from '../../config.js';
 
 
 export const registerUser = async (username, email, password) => {
   try {
-    const response = await fetch(`${API_BASE_URL}/api/users/register`, {
+    const response = await fetch(`${API_URL}/api/users/register`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
